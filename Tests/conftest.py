8a25df060cf51dfad197c6dadb35167950b04c1e
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-12-10 14:00:59 -0800

    ip_selenoid_serv

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -17,17 +17,9 @@ def browser(request):
     }
     capabilities = chrome
 
-    selenoid = request.config.getoption("selenoid")
-    if selenoid == "serv":
-        browser = webdriver.Remote(
-            command_executor="http://164.90.145.233:4444/wd/hub",
-            desired_capabilities=capabilities)
-    elif selenoid == "mac":
-        browser = webdriver.Remote(
-            command_executor="http://localhost:4444/wd/hub",
-            desired_capabilities=capabilities)
-    else:
-        raise pytest.UsageError("--selenoid should be mac or serv")
+    browser = webdriver.Remote(
+        command_executor="http://164.90.145.233:4444/wd/hub",
+        desired_capabilities=capabilities)
 
     browser.maximize_window()
     browser.implicitly_wait(t_out)
