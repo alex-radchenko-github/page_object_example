f51ef66f182cbb9465335a46095dd985dd979fe1
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-12-10 13:41:35 -0800

    ip_selenoid_serv

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium import webdriver
-from App.browsers import chrome, firefox, opera
 from App.remote_driver import ip_selenoid_mac, ip_selenoid_serv
 
 debug = True
@@ -11,21 +10,18 @@ t_out = 30
 def pytest_addoption(parser):
     parser.addoption('--selenoid', action='store', default='mac',
                      help="Choose selenoid type: serv or mac")
-    parser.addoption('--br_type', action='store', default='chrome',
-                     help="Choose br_type type: chrome, firefox or opera")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     br_type = request.config.getoption("br_type")
-    if br_type == "chrome":
-        capabilities = chrome
-    elif br_type == "firefox":
-        capabilities = firefox
-    elif br_type == "opera":
-        capabilities = opera
-    else:
-        raise pytest.UsageError("--br_type Choose should be chrome, firefox or opera")
+    chrome = {
+        "browserName": "chrome",
+        "version": "87.0",
+        "enableVNC": True,
+        "enableVideo": False
+    }
+    capabilities = chrome
 
     selenoid = request.config.getoption("selenoid")
     if selenoid == "serv":
