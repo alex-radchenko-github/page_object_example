cdc5b770429f82e96b0c451128f1023f79590585
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-12-12 12:12:48 -0800

    ip_selenoid_serv

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,8 +2,14 @@ import pytest
 from selenium import webdriver
 import App.browsers as browsers
 import App.remote_driver as remote_driver
-t_out = 30
 
+
+import logging
+import sys
+from pytest_reportportal import RPLogger, RPLogHandler
+
+
+t_out = 30
 
 def pytest_addoption(parser):
     parser.addoption('--selenoid', action='store', default='localhost',
@@ -43,3 +49,22 @@ def browser(request):
     yield browser
     browser.quit()
 
+@pytest.fixture(scope="session")
+def rp_logger(request):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    # Create handler for Report Portal if the service has been
+    # configured and started.
+    if hasattr(request.node.config, 'py_test_service'):
+        # Import Report Portal logger and handler to the test module.
+        logging.setLoggerClass(RPLogger)
+        rp_handler = RPLogHandler(request.node.config.py_test_service)
+        # Add additional handlers if it is necessary
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(logging.INFO)
+        logger.addHandler(console_handler)
+    else:
+        rp_handler = logging.StreamHandler(sys.stdout)
+    # Set INFO level for Report Portal handler.
+    rp_handler.setLevel(logging.INFO)
+    return logger
--- Tests/test_TestSmokeTesting.py
+++ Tests/test_TestSmokeTesting.py
@@ -6,12 +6,16 @@ from Pages.ProfilePage import ProfileHelper
 from selenium.webdriver.common.by import By
 import time
 
+import logging
+from pytest_reportportal import RPLogger, RPLogHandler
+
+
 debug = True
 
 @allure.feature("login")
 @allure.title("test_login_from_main_page_with_valid_credetials")
 @pytest.mark.smoke
-def test_login_from_main_page_with_valid_credetials(browser):
+def test_login_from_main_page_with_valid_credetials(browser, rp_logger):
     main_page = MainPageHelper(browser)
     main_page.go_to_site()
     main_page.full_login(accounts.acc["radwexe"])
@@ -21,7 +25,7 @@ def test_login_from_main_page_with_valid_credetials(browser):
 @allure.feature('login')
 @allure.title("login_from_token")
 @pytest.mark.smoke
-def test_login_from_token(browser):
+def test_login_from_token(browser, rp_logger):
     login_from_token = MainPageHelper(browser)
     login_from_token.go_to_site_through_token()
     login_from_token.login_check()
