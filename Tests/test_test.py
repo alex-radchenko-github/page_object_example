817c137c324d77444ecfddda92030785818a695b
alex rad <radchenko51.1@gmail.com>
2020-12-16 12:58:48 -0800

    TEST

--- Tests/test_test.py
+++ Tests/test_test.py
@@ -1,7 +1,16 @@
 import allure
 
 @allure.step
-def test_Test():
+def test_Test_111():
     assert 1+1 == 2
     print("1234")
 
+@allure.step
+def test_Test_222():
+    assert 1+1 == 2
+    print("1234")
+
+@allure.step
+def test_Test_333():
+    assert 1+1 == 2
+    print("1234")
