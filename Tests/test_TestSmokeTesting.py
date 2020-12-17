f5417534a1ae4615921f5b191b025fe19b495647
alex rad <radchenko51.1@gmail.com>
2020-12-16 18:43:40 -0800

    TEST

--- Tests/test_TestSmokeTesting.py
+++ Tests/test_TestSmokeTesting.py
@@ -12,13 +12,13 @@ def test_login_from_main_page(browser):
     main_page.full_login(accounts.acc["radwexe"])
     main_page.login_check()
 
-@allure.feature('login')
-@allure.title("login_from_token")
-@pytest.mark.smoke
-def test_login_from_token(browser):
-    test_login_from_token = MainPageHelper(browser)
-    test_login_from_token.go_to_site_through_token()
-    test_login_from_token.login_check()
+# @allure.feature('login')
+# @allure.title("login_from_token")
+# @pytest.mark.smoke
+# def test_login_from_token(browser):
+#     test_login_from_token = MainPageHelper(browser)
+#     test_login_from_token.go_to_site_through_token()
+#     test_login_from_token.login_check()
 
 @allure.feature("login11")
 @allure.title("login_from_main_page11")
@@ -29,13 +29,13 @@ def test_login_from_main_pag11(browser):
     main_page.full_login(accounts.acc["radwexe"])
     main_page.login_check()
 
-@allure.feature('login22')
-@allure.title("login_from_token22")
-@pytest.mark.smoke
-def test_login_from_token22(browser):
-    test_login_from_token = MainPageHelper(browser)
-    test_login_from_token.go_to_site_through_token()
-    test_login_from_token.login_check()
+# @allure.feature('login22')
+# @allure.title("login_from_token22")
+# @pytest.mark.smoke
+# def test_login_from_token22(browser):
+#     test_login_from_token = MainPageHelper(browser)
+#     test_login_from_token.go_to_site_through_token()
+#     test_login_from_token.login_check()
 
 @allure.feature("login33")
 @allure.title("login_from_main_page33")
@@ -46,13 +46,13 @@ def test_login_from_main_pag33(browser):
     main_page.full_login(accounts.acc["radwexe"])
     main_page.login_check()
 
-@allure.feature('login44')
-@allure.title("login_from_token44")
-@pytest.mark.smoke
-def test_login_from_token44(browser):
-    test_login_from_token = MainPageHelper(browser)
-    test_login_from_token.go_to_site_through_token()
-    test_login_from_token.login_check()
+# @allure.feature('login44')
+# @allure.title("login_from_token44")
+# @pytest.mark.smoke
+# def test_login_from_token44(browser):
+#     test_login_from_token = MainPageHelper(browser)
+#     test_login_from_token.go_to_site_through_token()
+#     test_login_from_token.login_check()
 
 @allure.feature("login55")
 @allure.title("login_from_main_page55")
@@ -63,13 +63,13 @@ def test_login_from_main_pag55(browser):
     main_page.full_login(accounts.acc["radwexe"])
     main_page.login_check()
 
-@allure.feature('login66')
-@allure.title("login_from_token66")
-@pytest.mark.smoke
-def test_login_from_token66(browser):
-    test_login_from_token = MainPageHelper(browser)
-    test_login_from_token.go_to_site_through_token()
-    test_login_from_token.login_check()
+# @allure.feature('login66')
+# @allure.title("login_from_token66")
+# @pytest.mark.smoke
+# def test_login_from_token66(browser):
+#     test_login_from_token = MainPageHelper(browser)
+#     test_login_from_token.go_to_site_through_token()
+#     test_login_from_token.login_check()
 
 @allure.feature("login77")
 @allure.title("login_from_main_page77")
