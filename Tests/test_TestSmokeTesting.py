c745feb7c3de6d00bdc0636ce70c951c778e12e8
alex-radchenko-github <650>
2021-01-15 18:22:46 -0800

    15.01.2021

--- Tests/test_TestSmokeTesting.py
+++ Tests/test_TestSmokeTesting.py
@@ -5,23 +5,23 @@ from Pages.MainPageLogin import MainPageHelper
 from allure_commons.types import AttachmentType
 
 
-# @allure.feature("login")
-# @allure.title("login_from_main_page")
-# @pytest.mark.smoke
-# def test_login_from_main_page(browser):
-#    main_page = MainPageHelper(browser)
-#    main_page.go_to_site()
-#    main_page.full_login(accounts.acc["radwexe"])
-#    main_page.login_check()
-#
-#
-# @allure.feature('login')
-# @allure.title("login_from_token")
-# @pytest.mark.smoke
-# def test_login_from_token(browser):
-#    test_login_from_token = MainPageHelper(browser)
-#    test_login_from_token.go_to_site_through_token()
-#    test_login_from_token.login_check()
+@allure.feature("login")
+@allure.title("login_from_main_page")
+@pytest.mark.smoke
+def test_login_from_main_page(browser):
+   main_page = MainPageHelper(browser)
+   main_page.go_to_site()
+   main_page.full_login(accounts.acc["radwexe"])
+   main_page.login_check()
+
+
+@allure.feature('login')
+@allure.title("login_from_token")
+@pytest.mark.smoke
+def test_login_from_token(browser):
+   test_login_from_token = MainPageHelper(browser)
+   test_login_from_token.go_to_site_through_token()
+   test_login_from_token.login_check()
 
 @allure.epic("epic_epic")
 @allure.feature('feature_feature')
