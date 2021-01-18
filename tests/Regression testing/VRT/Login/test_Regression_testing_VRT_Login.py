3312cb08196315ed2e93d69d8a0f0602c8606bad
alex-radchenko-github <650>
2021-01-17 19:48:29 -0800

    VRT + AT-8

--- tests/Regression testing/VRT/Login/test_Regression_testing_VRT_Login.py
+++ tests/Regression testing/VRT/Login/test_Regression_testing_VRT_Login.py
@@ -16,7 +16,8 @@ def test_vrt_main_page_click_on_the_yellow_button(browser):
     main_page.go_to_site("/")
     main_page.click_on_the_yellow_button()
     time.sleep(3)
-    main_page.screenshot_check('AT-7_test_vrt_main_page_click_on_the_yellow_button', '1920x964', 'linux','selenoid')
+    main_page.screenshot_check('AT-7_test_vrt_main_page_click_on_the_yellow_button', '1920x964', 'linux', 'selenoid')
+
 
 @allure.testcase("https://app.qase.io/case/AT-8")
 @allure.feature("login")
@@ -29,4 +30,5 @@ def test_vrt_main_page_modal_window_top_right_button_with_valid_credentials_befo
     main_page.click_on_the_yellow_button()
     main_page.enter_email("radwexe@mail.ru")
     main_page.enter_password("111")
-    main_page.screenshot_check('AT-8_vrt_main_page_modal_window_top_right_button_with_valid_credentials_before_click', '1920x964', 'linux','selenoid')
+    main_page.screenshot_check('AT-8_vrt_main_page_modal_window_top_right_button_with_valid_credentials_before_click',
+                               '1920x964', 'linux', 'selenoid')
